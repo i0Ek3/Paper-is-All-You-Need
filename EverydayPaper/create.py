45901ed6694253fd679e04e3f70e40de220e24e6
i0Ek3 <kno30826@gmail.com>
2024-08-22 22:19:38 +0800

    Update files

---
+++ EverydayPaper/create.py
@@ -0,0 +1,22 @@
+import os
+from datetime import datetime
+
+today = datetime.now().strftime('%Y%m%d')
+
+folder_path = os.path.join(os.getcwd(), today)
+
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+    print(f"Dir '{folder_path}' created.")
+else:
+    print(f"Dir '{folder_path}' existed.")
+
+md_file_path = os.path.join(folder_path, f"{today}.md")
+
+if not os.path.isfile(md_file_path):
+    with open(md_file_path, 'w') as md_file:
+        md_file.write('# ' + today + '\n\n')
+        md_file.write("> This markdown file created on " + today + ".\n")
+    print(f"Markdown '{md_file_path}' created.")
+else:
+    print(f"Markdown '{md_file_path}' existed.")
--- EverydayReading/create_md.py
+++ EverydayReading/create_md.py
@@ -2,18 +2,14 @@ import os
 from datetime import datetime
 
 def create_markdown_file_if_not_exists(path):
-    # 获取当天日期并格式化为 yyyymmdd
     today = datetime.now().strftime("%Y%m%d")
     filename = f"{today}.md"
 
-    # 确定完整文件路径
     file_path = os.path.join(path, filename)
 
-    # 检查文件是否存在
     if os.path.exists(file_path):
         print(f"File already exists: {file_path}")
     else:
-        # 创建文件并写入一些初始内容
         with open(file_path, 'w') as file:
             file.write(f"# {today}\n\n")
             file.write("> This markdown file created on " + today + ".\n")
